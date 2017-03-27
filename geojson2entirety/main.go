6a693d2ab196ed3f249701895367188983c5ad35
donomii <donomii+github@gmail.com>
2017-03-27 08:27:00 +0100

    Improve error handling

--- geojson2entirety/main.go
+++ geojson2entirety/main.go
@@ -47,28 +47,61 @@ func check(err error) {
 	}
 }
 
-func unpackJSON(accum []byte) geojson.Container {
+func checkJSONerr(err error, js []byte) {
+	if err != nil {
+		log.Println(err)
+		log.Println(string(js))
+	}
+}
+
+func unpackJSON(accum []byte) (geojson.Container, error) {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("Caught error in unpackJSON ", r)
-			//return geojson.Container{}
+			//return geojson.Container{}, nil
 		}
 	}()
 	result := geojson.Container{}
 	err := json.Unmarshal(accum, &result)
-	check(err)
-	return result
+	checkJSONerr(err, accum)
+	if err != nil {
+		return geojson.Container{}, err
+	}
+	return result, nil
 }
+
+func writeTag(str string, lat, long float64, tagpointsFile, offsetFile, indexFile, tagcatFile, stringsFile, preoffsetFile *os.File, indexCount, offset int64) int64 {
+
+	//fmt.Println("Parsed: ", string2Bytes(result.Properties["name"].(string)))
+	//fmt.Printf("%s ", string2Bytes(result.Properties["name"].(string)))
+
+	//str = strings.Replace(str, "\"", "\\\"", -1)
+	if verbose {
+		log.Println("Adding tag ", indexCount, ": ", str, " at ", lat, ",", long, " at offset ", offset)
+	}
+	outBytes, blength := string2Bytes(str)
+	wrote, err := stringsFile.Write(outBytes)
+	if wrote != blength {
+		panic("Written length is different from string length!")
+	}
+	check(err)
+	writeBytesInt(int64(offset), offsetFile)
+	_, err = preoffsetFile.Write([]byte(fmt.Sprintf("%v\n", offset)))
+
+	writeBytes(lat, tagpointsFile)
+	writeBytes(long, tagpointsFile)
+	writeBytesInt(indexCount-1, indexFile)
+	writeBytesInt(0, tagcatFile)
+	return int64(blength)
+}
+
+var verbose bool
 
 func main() {
 	var mapName = flag.String("outFile", "default_map", "Name for map file")
-	var limit = flag.Int("limit", -1, "Limit the number of records imported")
+	var limit = flag.Int64("limit", -1, "Limit the number of records imported")
+	verbose = *flag.Bool("verbose", false, "Print progress")
 	//var skip = flag.Int("skip", -1, "Skip every nth record")
-	/*var mapName string
-	if len(os.Args) > 1 {
-		mapName = os.Args[1]
-		log.Println(mapName)
-	}*/
 
 	flag.Parse()
 	var err error
@@ -100,8 +133,10 @@ func main() {
 	line := []byte{}
 	more := false
 	accum := []byte{}
-	offset := 0
-	count := 0
+	offset := int64(0)
+	count := int64(0)
+	indexCount := int64(0)
+	offset += writeTag("FAIL", -60000, -6000, tagpointsFile, offsetFile, indexFile, tagcatFile, stringsFile, preoffsetFile, indexCount, offset)
 	for {
 
 		line, more, err = scanner.ReadLine()
@@ -110,49 +145,44 @@ func main() {
 			log.Println("Done.  Finished map: ", *mapName)
 			os.Exit(0)
 		}
+		accum = append(accum, line...)
 		if more {
-			accum = append(accum, line...)
+			//accum = append(accum, line...)
 		} else {
 			if *limit > -1 && count > *limit {
 				log.Printf("Finishing import early after %d records for %v", count, *mapName)
 				os.Exit(0)
 			}
-			count = count + 1
-			accum = append(accum, line...)
+
+			//accum = append(accum, line...)
 			//fmt.Printf("Line: %v\n", string(accum[:len(accum)]))
-			result := unpackJSON(accum)
+			result, err := unpackJSON(accum)
 			check(err)
-			/*if *skip > -1 {
-			if count >= *skip {
-				count = 0
-			} else {*/
-			if result.Properties["name"] != nil && len(result.Properties["name"].(string)) > 1 {
+			if err == nil {
 
-				//fmt.Println("Parsed: ", string2Bytes(result.Properties["name"].(string)))
-				//fmt.Printf("%s ", string2Bytes(result.Properties["name"].(string)))
-				str := result.Properties["name"].(string)
-				//log.Println("Adding tag: ", str)
-				//str = strings.Replace(str, "\"", "\\\"", -1)
-				outBytes, blength := string2Bytes(str)
-				_, err = stringsFile.Write(outBytes)
-				check(err)
-				writeBytesInt(int64(offset), offsetFile)
-				_, err = preoffsetFile.Write([]byte(fmt.Sprintf("%v\n", offset)))
-				offset += blength
-
-				writeBytes(result.Geometry.Point[1]*60, tagpointsFile)
-				writeBytes(result.Geometry.Point[0]*-60, tagpointsFile)
-				writeBytesInt(count-1, indexFile)
-				writeBytesInt(0, tagcatFile)
+				/*if *skip > -1 {
+				if count >= *skip {
+					count = 0
+				} else {*/
+				if result.Properties["name"] != nil && len(result.Properties["name"].(string)) > 1 {
+					count = count + 1
+					indexCount += 1
+					str := result.Properties["name"].(string)
+					offset += writeTag(str, result.Geometry.Point[1]*60, result.Geometry.Point[0]*-60, tagpointsFile, offsetFile, indexFile, tagcatFile, stringsFile, preoffsetFile, indexCount, offset)
+				} else {
+					if verbose {
+						fmt.Println("Adding point without tag at ", result.Geometry.Point)
+					}
+					writeBytes(result.Geometry.Point[1]*60, pointsFile)
+					writeBytes(result.Geometry.Point[0]*-60, pointsFile)
+					writeBytes(0, pointdataFile)
+					writeBytes(0, pointdataFile)
+					writeBytes(0, pointdataFile)
+				}
+				accum = []byte{}
 			} else {
-				//fmt.Println("Adding point without tag")
-				writeBytes(result.Geometry.Point[1]*60, pointsFile)
-				writeBytes(result.Geometry.Point[0]*-60, pointsFile)
-				writeBytes(0, pointdataFile)
-				writeBytes(0, pointdataFile)
-				writeBytes(0, pointdataFile)
+				accum = []byte{}
 			}
-			accum = []byte{}
 		}
 	}
 
